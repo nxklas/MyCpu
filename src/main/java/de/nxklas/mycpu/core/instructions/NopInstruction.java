d30479814744f7c686324d5dea1a8e99a88b4ce9
nxklas <51093729+nxklas@users.noreply.github.com>
2026-05-16 22:44:29 +0200

    Remove properties of NopInstruction

--- src/main/java/de/nxklas/mycpu/core/instructions/NopInstruction.java
+++ src/main/java/de/nxklas/mycpu/core/instructions/NopInstruction.java
@@ -1,6 +1,4 @@
 package de.nxklas.mycpu.core.instructions;
 
-import de.nxklas.mycpu.core.operands.Operand;
-
-public final record NopInstruction(Operand dst, Operand src) implements Instruction {
+public final record NopInstruction() implements Instruction {
 }
